6fffe3410902d6284a5467ee5819efc6b61f3cba
Abokashefmohamed <abokashefmohamed@gmail.com>
2026-06-21 14:14:21 +0200

    feat: connect to MongoDB using mongoose

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 const PORT = 3000;
@@ -7,6 +11,8 @@ const PORT = 3000;
 app.get("/", (req, res) => {
     res.send("Hello from the backend!");
 });
+
+await connectDB();
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
